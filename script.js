eefc6e31f24cdc2ff2f47a5b8da88342e3714cf4
uabcampos <ccampos@uabmc.edu>
2024-08-09 10:56:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,28 @@ document.getElementById('name').addEventListener('input', function() {
     document.getElementById('preview-name').innerText = this.value;
 });
 
+document.getElementById('credentials').addEventListener('input', function() {
+    document.getElementById('preview-credentials').innerText = this.value;
+});
+
 document.getElementById('title').addEventListener('input', function() {
     document.getElementById('preview-title').innerText = this.value;
 });
 
-document.getElementById('location').addEventListener('input', function() {
-    document.getElementById('preview-location').innerText = this.value;
+document.getElementById('room').addEventListener('input', function() {
+    document.getElementById('preview-room').innerText = this.value;
+});
+
+document.getElementById('street').addEventListener('input', function() {
+    document.getElementById('preview-street').innerText = this.value;
+});
+
+document.getElementById('city-state').addEventListener('input', function() {
+    document.getElementById('preview-city-state').innerText = this.value;
+});
+
+document.getElementById('zip').addEventListener('input', function() {
+    document.getElementById('preview-zip').innerText = this.value;
 });
 
 document.getElementById('phone').addEventListener('input', function() {
